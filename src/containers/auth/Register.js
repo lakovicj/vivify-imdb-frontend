a3e6f2a9b10ae9ee816af360d78719b671fe44e7
Jovan Lakovic <jovan.lakovic@vivifyideas.com>
2020-12-10 16:22:08 +0100

    Bugfix: Register validation added

--- src/containers/auth/Register.js
+++ src/containers/auth/Register.js
@@ -1,55 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-
+import { Formik, Form, Field } from 'formik';
 import { register } from '../../store/actions/AuthActions';
+import { registerSchema } from '../forms/validation/authSchemas';
 
 class Register extends Component {
-  state = {
-    email: '',
-    password: '',
-    name: ''
-  };
 
-  handleInputChange = field => event => this.setState({ [field]: event.target.value });
+  submit = (data, setSubmitting) => {
+    console.log(data);
+    const {name, email, password} = data;
+    const registerData = {name, email, password}
 
-  submit = event => {
-    event.preventDefault();
-
-    let registerData = {
-      email: this.state.email,
-      password: this.state.password,
-      name: this.state.name
-    };
-    this.props.register(registerData);
-  };
+    this.props.register(registerData)
+    setSubmitting(false);
+  }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.submit}>
-          <h2>Register</h2>
-          <input
-            type="text"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleInputChange('email')}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleInputChange('password')}
-          />
-          <input
-            type="text"
-            placeholder="Name"
-            value={this.state.name}
-            onChange={this.handleInputChange('name')}
-          />
-          <input type="submit" value="Register" />
-          {this.props.registerError && <p>registerError</p>}
-        </form>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+          }} 
+          validationSchema={registerSchema} 
+          onSubmit={(values, {setSubmitting}) => this.submit(values, setSubmitting)}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <article className="card-body">
+              <Form autoComplete="off"> 
+                <div className="form-row">
+                  <Field type="text" name="name" placeholder="Name"/>
+                  {touched.name && errors.name && <p>{errors.name}</p>}
+                </div>
+                <div className="form-row">
+                  <Field type="email" name="email" placeholder="Email"/>
+                  {touched.email && errors.email && <p>{errors.email}</p>}
+                </div>
+                <div className="form-row">  
+                  <Field type="password" name="password" placeholder="Password"/>
+                  {touched.password && errors.password && <p>{errors.password}</p>}
+                </div>  
+                <div className="form-row">
+                  <Field type="password" name="confirmPassword" placeholder="Confirm password"/>
+                  {touched.confirmPassword && errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+                </div> 
+                {this.props.registerError && <p>Email is already taken</p>}
+                <button disabled={isSubmitting} type="submit">Register</button>
+              </Form>
+            </article>
+          )}
+        </Formik>
       </div>
     );
   }
