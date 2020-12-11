b25255dad670f3d09d52161cb371c7c8a4144f23
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-12-11 12:59:19 +0100

    JLPI-26: Movie list styles (#3)
    New component MovieList is rendering MovieCard items
    
    Co-authored-by: Jovan Lakovic <jovan.lakovic@vivifyideas.com>

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MovieCard = ({ movie }) => {
+
+  const sliceDescription = description => {
+    return description.length > 100 ? description.slice(0, 150) + "..." : description;
+  }
+
+  const getMovieGenre = movie => {
+    return movie.genre ? movie.genre.name : "no genre";
+  }
+
   return (
-    <div>
-      <p>{movie.title}</p>
-    </div>
+      <div className="card mb-4">
+        <img className="card-img-top" src={movie.image_url} alt="Card image cap"/>
+        <div className="card-body">
+          <h5 className="card-title">{movie.title}</h5>
+          <p className="card-text">{sliceDescription(movie.description)}</p>
+        </div>
+        <div className="card-footer">
+            <small className="text-muted">{getMovieGenre(movie)}</small>
+        </div>
+      </div>
+    
   );
 };
 
---
+++ src/component/MovieList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import MovieCard from './MovieCard';
+
+export class MovieList extends Component {
+
+    movieCards = () => {
+        return this.props.movies.map((m, i) => 
+            <div key={m.id} className="col-md-4 d-flex">
+                <MovieCard movie={m} />
+            </div>
+        )
+    }
+
+    createLayout = () => {
+        return (
+            <div className="row">
+                <div className="card-deck">
+                    {this.movieCards()}
+                </div>
+            </div>
+        )
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {this.createLayout()}
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = state => {
+    return {
+      movies: state.movie.all
+    };
+  };
+
+export default connect(mapStateToProps)(MovieList);
