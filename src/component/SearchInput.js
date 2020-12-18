729091112bbc01b4853080a67a60517d597639fd
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-12-18 10:46:07 +0100

    JLPI-2: Movie Seach (#7)
    Co-authored-by: Jovan Lakovic <jovan.lakovic@vivifyideas.com>

---
+++ src/component/SearchInput.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { debounce } from 'lodash'
+
+export class SearchInput extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchInput: '',
+            perPage: 3,
+            page: 1
+        }
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.search = debounce(this.search, 750);
+        this.getAllMovies = debounce(this.getAllMovies, 750);
+        
+    }
+    
+    search = (param) => {
+        this.props.searchMovies({
+            page: this.state.page,
+            perPage: this.state.perPage,
+            title: param
+        })
+    }
+
+    getAllMovies = () => {
+        this.props.getMoviesByPage({
+            page: this.state.page,
+            perPage: this.state.perPage
+        });
+    }
+    
+
+    handleOnChange(e) {     
+        this.setState({
+            searchInput: e.target.value
+        });
+        if (e.target.value === '') {
+            this.getAllMovies();
+        } else {
+            this.search(e.target.value);
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    className="input-group" 
+                    type="text"
+                    name="searchInput"
+                    placeholder="Search..."
+                    value={this.state.searchInput}
+                    onChange={this.handleOnChange}/>
+            </div>
+        )
+    }
+}
+
+export default SearchInput
